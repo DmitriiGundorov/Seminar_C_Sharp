d68902bc6d1d04270dd4da1da0b0720c8469a275
Dmitrii Gundorov <gundorov1987@yandex.ru>
2022-12-04 23:08:45 +0300

    Examples_21

--- Examples_Seminar_3/Program.cs
+++ Examples_Seminar_3/Program.cs
@@ -32,7 +32,7 @@ void Example_17()
 }
 //Example_17();
 
-// Задача 18. Напишите программу, которая по заданному номеру четверти, 
+// Задача 18. Напишите программу, которая по заданному номеру четверти,
 // показывает диапазон возможных координат точек в этой четверти (x и y).
 
 void Example_18()
@@ -52,13 +52,37 @@ void Example_18()
     {
         Console.WriteLine("Значение координат x < 0 и у < 0");
     }
-    else if (number == 4)
-    {
-        Console.WriteLine("Значение координат x > 0 и у < 0");
-    }
-    else if (number > 4 || number < 1)
+    else if (number == 4) Console.WriteLine("Значение координат x > 0 и у < 0"); // Запись в строку, возможно делать если условие одно. 
+
+    // else if (number > 4 || number < 1)
+    else
     {
         Console.WriteLine("Введено некорректное значение");
     }
 }
-Example_18();
+//Example_18();
+
+// Задача 21. Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 2D пространстве.
+
+void Example_21()
+{
+    //int x=5;
+    //Math.Pow(x, 2); - возведение в квадрат
+    //Math.Sqrt(x, 2); - корень в квадрате
+    //Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+    Console.WriteLine("Задача 21");
+    Random rand = new Random();
+    double x1 = rand.Next(-10, 11);
+    double y1 = rand.Next(-10, 11);
+    Console.WriteLine($"Координаты точки A ( {x1}, {y1} )");
+    double x2 = rand.Next(-10, 11);
+    double y2 = rand.Next(-10, 11);
+    Console.WriteLine($"Координаты точки В ( {x2}, {y2} )");
+    double Distanse = 0;
+    Distanse = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    Distanse = Math.Round(Distanse, 2);// Округление числа до 2-го знака после запятой
+    Console.WriteLine($"Расстояние между точкой А и В (Длина отрезка АВ): {Distanse}");
+}
+Example_21();
