7aac092fca654208aa661347d3e418d9eedb73e6
Dmitrii Gundorov <gundorov1987@yandex.ru>
2022-12-07 06:41:39 +0300

    Example_19

---
+++ Homework_Seminar_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход
+// пятизначное число и проверяет, является ли оно палиндромом (число, которое читается слева направо и справо налево одинаково).
+
+void Example_19()
+{
+    Console.WriteLine("|    Задача 22     |");
+    Console.WriteLine("Введите пятизначное целое число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number < 10000 | number > 100000) Console.WriteLine("Некорректный ввод");
+    else
+    {
+
+    }
+    //Random rand = new Random();
+    //int number = rand.Next(10000, 99999);
+    //Console.WriteLine(number);
+
+
+
+}
+Example_19();
