8a6c3b5d1de514732aa8b257f84b1bc226e62930
Dmitrii Gundorov <gundorov1987@yandex.ru>
2022-12-11 22:58:37 +0300

    HomeWork_Seminar_5

--- HomeWork_Seminar_5/Program.cs
+++ HomeWork_Seminar_5/Program.cs
@@ -39,7 +39,6 @@ void Example_34()
     }
     Console.WriteLine($"Количество четных чисел в массиве - > {count}");
 }
-//Example_34();
 
 // Задача 36: Задайте одномерный массив, заполненный случайными
 // числами. Найдите сумму элементов с нечётными индексами.
@@ -47,11 +46,52 @@ void Example_34()
 void Example_36()
 {
     Console.WriteLine("|    Задача 36     |");
-    int size = 912;
-
-    Console.WriteLine(size % 2);
+    int size = 10;
+    int sum = 0;
+    int[] numbers = new int[size];
+    FillArray(numbers, 0, 10);
+    Console.Write("Заданный массив:  ");
+    PrintArray(numbers);
+    for (int i = 1; i < size; i += 2)
+    {
+        sum += numbers[i];
+    }
+    Console.WriteLine($"Сумма элементов с нечетными индексами массива - > {sum}");
 }
-//Example_36();
 
 // Задача 38: Задайте массив вещественных чисел. Найдите 
 // разницу между максимальным и минимальным элементов массива.
+
+void Example_38()
+{
+    Console.WriteLine("|    Задача 38     |");
+    int size = 6;
+    double[] numbers = new double[size];
+    Random rand = new Random();
+    double delitel = 100;
+    double numberMax = 0;
+    double numberMin = 100;
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = rand.Next(1000, 10000) / delitel;
+    }
+    Console.WriteLine("   Заданный массив:");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(numbers[i] + "  ");
+    }
+    Console.WriteLine();
+    //double numberMin = numbers[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (numbers[i] > numberMax) numberMax = numbers[i];
+        if (numbers[i] <= numberMin) numberMin = numbers[i];
+    }
+    Console.WriteLine("Минимальное значение массива:  " + numberMin);
+    Console.WriteLine("Максимальное значение массива:  " + numberMax);
+    Console.WriteLine($"Разница между минимальным и максимальным значением массива - > {numberMax - numberMin}");
+}
+
+Example_34();
+Example_36();
+Example_38();
